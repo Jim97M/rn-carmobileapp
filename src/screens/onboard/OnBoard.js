b65652949da2f7a9f46bbe171b63214981ca4060
Jim97M <wabuyajames@gmail.com>
2022-07-22 10:33:35 +0300

    Onboard Page

--- src/screens/onboard/OnBoard.js
+++ src/screens/onboard/OnBoard.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable quotes */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable no-unused-vars */
@@ -8,33 +10,39 @@ import {
     Dimensions,
     Image,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
-const {height, width} = Dimensions.get('window'); 
+import { FlatList } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-navigation';
+
+const COLORS = {primary: '#282534', white: '#fff'};
+
+const {height, width} = Dimensions.get('window');
 const slideItems = [
     {
         id: 1,
         title: "Best Cars",
         description: "Find Car To Book",
-        image: "../../images/car_sleek.png"
+        image: "../../images/car_sleek.png",
     },
     {
         id: 2,
         title: "Book Car",
         description: "Rent Easy",
-        image: "../../images/car_saloon.png"
+        image: "../../images/car_saloon.png",
     },
     {
         id: 1,
         title: "Start Now",
         description: "Lets Get Started",
-        image: "../../images/car_red.png"
+        image: "../../images/car_red.png",
     },
 
-]
+];
 
 const Slide = ({item}) => {
-    return(
+    return (
       <View style={{alignItems: 'center'}}>
            <Image source={item?.image}
             style={{height: '75%', width, resizeMode: 'contain'}}
@@ -44,8 +52,8 @@ const Slide = ({item}) => {
               <Text style={styles.description}>{item?.description}</Text>
            </View>
       </View>
-    )
-}
+    );
+};
 
 const OnBoard = ({navigation}) => {
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
@@ -59,26 +67,130 @@ const OnBoard = ({navigation}) => {
 
     const goToNextSlide = () => {
         const nextSlideIndex = currentSlideIndex + 1;
-        if(nextSlideIndex != slideItems.length){
+        if (nextSlideIndex != slideItems.length){
             const offset = nextSlideIndex * width;
             ref?.current.scrollToOffset({offset});
             setCurrentSlideIndex(currentSlideIndex + 1);
         }
-    }
+    };
 
     const skip = () => {
         const lastSlideIndex = slideItems.length - 1;
         const offset = lastSlideIndex * width;
         ref?.current.scrollToOffset({offset});
         setCurrentSlideIndex(lastSlideIndex);
-    }
+    };
+
+    const Footer = () => {
+        return (
+            <View
+             style={{
+                height: height * 0.25,
+                justifyContent: 'space-between',
+                paddingHorizontal: 20,
+             }}>
+               {/* Indicator Container */}
+               <View
+                 style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    marginTop: 20,
+                 }}>
+                    {/* Indicator Container */}
+                   {slideItems.map((_, index) => (
+                    <View
+                     key={index}
+                     style={[
+                        styles.indicator,
+                        currentSlideIndex == index && {
+                           backgroundColor: COLORS.white,
+                           width: 25,
+                        },
+                     ]}
+                    />
+                   ))
+
+                   }
+               </View>
+
+             {/* Render Buttons */}
+             <View style={{marginBottom: 20}}>
+                 {currentSlideIndex == slideItems.length - 1 ? (
+                    <View style={{height: 50}}>
+                        <TouchableOpacity
+                           style={styles.btn}
+                           onPress={() => navigation.navigate('SignUpScreen')}
+                        >
+                            <Text
+                              style={{fontWeight: 'bold', fontSize: 15}}
+                            >
+                                GET STARTED
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                 ) : (
+                    <View style={{flexDirection: 'row'}}>
+                        <TouchableOpacity
+                          activeOpacity={0.8}
+                          style={[
+                            styles.btn,
+                            {
+                                borderColor: COLORS.white,
+                                borderWidth: 1,
+                                backgroundColor: 'transparent',
+                            },
+                          ]}
+                          onPress={skip}
+                         >
+                         <Text
+                           style={{
+                            fontWeight: 'bold',
+                            fontSize: 15,
+                            color: COLORS.white
+                           }}>
+                            SKIP
+                         </Text>
+                        </TouchableOpacity>
+                        <View style={{width: 15}} />
+                         <TouchableOpacity
+                           activeOpacity={0.8}
+                           onPress={goToNextSlide}
+                           style={styles.btn}>
+                           <Text
+                            style={{
+                                fontWeight: 'bold',
+                                fontSize: 15,
+                            }}
+                           >
+                             NEXT
+                           </Text>
+                         
+                            
+                         </TouchableOpacity>   
+                    </View>
+                 )}
+             </View>
+            </View>
+        );
+    };
 
   return (
-    <View>
-          
-    </View>
-  )
-}
+    <SafeAreaView  style={{flex: 1, backgroundColor: COLORS.primary}}>
+      <StatusBar backgroundColor={COLORS.primary} />
+      <FlatList
+        ref={ref}
+        onMomentumScrollEnd={updateCurrentSlideIndex}
+        contentContainerStyle={{height: height * 0.75}}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={slideItems}
+        pagingEnabled
+        renderItem={({item}) => <Slide item={item} />}
+      />
+      <Footer />
+    </SafeAreaView>
+  );
+};
 
 export default OnBoard;
 
@@ -88,13 +200,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 20,
         color: 'white',
-        textAlign: 'center'
+        textAlign: 'center',
     },
     description: {
         color: 'white',
         fontSize: 13,
         textAlign: 'center',
-        marginTop: 10
+        marginTop: 10,
     },
     image: {
         height: '80%',
@@ -116,4 +228,4 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
       },
-})
+});
